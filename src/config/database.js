efea51dde6346ada9428c56817c5418bd27c374d
Carolina <carolinamartinsfreitas@gmail.com>
2021-04-17 12:23:57 -0300

    atualização no database

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose'); //importa mongoose
-
-mongoose.connect('mongodb://localhost/tarefas', { useMongoClient: true }); //conecta ao banco de dados "tarefas"
 mongoose.Promise = global.Promise; 
+
+mongoose.connect('mongodb://localhost/tarefas', { //conecta ao banco de dados "tarefas"
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -19,7 +19,14 @@ app.use(cors());
 app.use(bodyParser.json()); //possibilita uso de arquivos JSON
 app.use(bodyParser.urlencoded({extended: false})); 
 
-require('../controllers/authController')(app); //referencia o controller de autenticação
-require('../controllers/taskController')(app);
+require('./controllers/authController')(app); //referencia o controller de autenticação
+require('./controllers/taskController')(app);
 
-app.listen(3000); //indica a porta
+app.get('/', (req,res) => {
+  res.send('olá');
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`iniciando na porta ${port}`);
+});

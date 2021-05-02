6f88922db95aced8e998db0d4501447c25cbb927
Balbino <cecyliasb@gmail.com>
2021-05-02 14:31:16 -0300

    algumas mudancas2

--- src/routes/task.js
+++ src/routes/task.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 const auth = require('../middleware/auth');
-router.use(auth.authentication);
+//router.use(auth.authentication);
 
-router.get('/allTasks', taskController.allTasks);
-router.post('/addTask/:id', taskController.addTask);
-router.patch('/update/:id', taskController.updateTask);
-router.delete('/remove/:id', taskController.removeTask);
+router.get('/allTasks', auth.authentication, taskController.allTasks);
+router.post('/addTask/:id', auth.authentication, taskController.addTask);
+router.patch('/update/:id', auth.authentication, taskController.updateTask);
+router.delete('/remove/:id', auth.authentication, taskController.removeTask);
 
 module.exports = router;

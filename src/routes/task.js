3026f247ec1dbd2ad34b1f70e1333d222e98d254
Balbino <cecyliasb@gmail.com>
2021-05-02 15:21:51 -0300

    algumas mudancas4

--- src/routes/task.js
+++ src/routes/task.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 //router.use(auth.authentication);
 
 router.get('/allTasks', auth.authentication, taskController.allTasks);
-router.get('/:id', auth.authentication, taskController.allTasks);
+router.get('/:id', auth.authentication, taskController.findById);
 router.post('/addTask/:id', auth.authentication, taskController.addTask);
 router.patch('/update/:id', auth.authentication, taskController.updateTask);
 router.delete('/remove/:id', auth.authentication, taskController.removeTask);
